2a24bd470671c7ee16554b14d5cc8a149855238b
Jonathan Jenne <johnnie@posteo.de>
2018-04-15 12:47:46 +0200

    show cost of buildings, don't show build button if unique :art:

--- src/components/Houses.js
+++ src/components/Houses.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
+import _ from 'lodash'
 
 export default ({ houses, build }) => (
 	<div>
 		{houses.map(house => (
-			<div key={house.name}>
-				<div>
-					{house.name}: {house.count}x ({house.working} working)
+			<div
+				key={house.name}
+				className="columns"
+				title={`${house.count}x (${house.working} working)`}
+			>
+				<div className="column">
+					<div>
+						{house.prettyName}: {house.count}/{house.working}
+					</div>
+					<div>
+						{_.map(house.cost, (value, resource) => (
+							<div>
+								{resource}: {value}
+							</div>
+						))}
+					</div>
 				</div>
-				<button
-					onClick={e => {
-						e.preventDefault()
-						build(house.name)
-					}}
-				>
-					Build one
-				</button>
+				<div className="column">
+					{!house.unique && (
+						<button
+							onClick={e => {
+								e.preventDefault()
+								build(house.name)
+							}}
+						>
+							Build
+						</button>
+					)}
+				</div>
 			</div>
 		))}
 	</div>
